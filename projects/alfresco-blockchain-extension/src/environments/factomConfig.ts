5d2250ae2365afbf50faf94b37150869fe462ace
sander <spostma@sphereon.com>
2018-11-16 16:13:33 +0000

    SPMS-281: Intake commit

---
+++ projects/alfresco-blockchain-extension/src/environments/factomConfig.ts
@@ -0,0 +1,12 @@
+/**
+ * These values are pointing to a Factom test net, do not use for production
+ */
+
+export const factomConfig = {
+  protocol: 'https',
+  factomdHost: 'factomd.testnet.sphereon.com',
+  factomdPort: 443,
+  walletdHost: 'walletd.testnet.sphereon.com',
+  walletdPort: 443,
+  entryCreditAddress: 'EC3cqLZPq5ypwRB5CLfXnud5vkWAV2sd235CFf9KcWcE3FH9GRxv'
+};
